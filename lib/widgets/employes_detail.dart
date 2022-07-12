28794106d18289b49adea54ab81f3765d4d2ac93
Sharang-1 <76429390+Sharang-1@users.noreply.github.com>
2022-07-12 21:46:14 +0530

    Add files via upload

---
+++ lib/widgets/employes_detail.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Employeedetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('employee name'),
+      ),
+      body: Center(child: Text('data')),
+    );
+  }
+}
